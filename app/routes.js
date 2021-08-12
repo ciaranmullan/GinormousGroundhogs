a45b1f90f4f227aa353025218b636e603432da52
Callum Kindred <callum-luis.kindred@kainos.com>
2021-08-12 15:51:50 +0100

    use cases 1 & 3 done

--- app/routes.js
+++ app/routes.js
@@ -1,16 +1,23 @@
 const express = require('express')
 const router = express.Router()
+const employeedata = require('./employeedata.js')
 
 // Add your routes here - above the module.exports line
-
-router.get('/sales_emp_check_answers', (req, res) => {
-    res.render('sales_emp_check_answers')
+router.get("/submitemployee", async (req, res) => {
+    console.log("body: " + JSON.stringify(req.session))
+    res.render('employee_submit', { result: await employeedata.insertEmployee(req.session.data, "technical") })
 });
 
-router.get('/employee_by_department', async (req, res) => { 
-    res.render('employee_by_department', { employees: await employeedata.getEmployeesByDepartment() } );
-    testTable = employeedata.getEmployeesByDepartment() 
-    console.log(testTable); 
+router.get("/submitsalesemp", async (req, res) => {
+    console.log("body: " + JSON.stringify(req.session.data))
+    res.render('sales_emp_submit', { result: await employeedata.insertSalesEmployee(req.session.data) })
+});
+
+
+router.get('/employee_by_department', async (req, res) => {
+    res.render('employee_by_department', { employees: await employeedata.getEmployeesByDepartment() });
+    testTable = employeedata.getEmployeesByDepartment()
+    console.log(testTable);
 });
 
 router.get('/createemployee', (req, res) => {
@@ -27,26 +34,28 @@ router.get('/createsalesemp', (req, res) => {
     res.render('create_sales_employee')
 });
 
-
+router.get('/sales_emp_check_answers', (req, res) => {
+    res.render('sales_emp_check_answers')
+});
 
 router.get('/:team', (req, res) => {
-    if(req.params.team == 'hr'){
+    if (req.params.team == 'hr') {
         res.render('hr_options')
-    } else if(req.params.team == 'finance'){
+    } else if (req.params.team == 'finance') {
         res.render('fn_options')
 
-    } else if(req.params.team == 'sales'){
+    } else if (req.params.team == 'sales') {
         res.render('s_options')
-        
-    } else if(req.params.team == 'talent'){
+
+    } else if (req.params.team == 'talent') {
         res.render('tm_options')
-        
+
     }
-   
+
 });
 
 
 module.exports = router
 
-const employeedata = require('./employeedata.js')
+
 
