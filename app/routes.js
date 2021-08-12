d3ca23d834a4ea8e3f99fb24f1809a351796d3a9
Eman Abbasi <eman.abbasi@kainos.com>
2021-08-12 10:37:48 +0100

    updated routes.js - no more redundent code

--- app/routes.js
+++ app/routes.js
@@ -3,18 +3,6 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
-router.get('/finance', (req, res) => {
-    res.render('fn_options', { 
-   } );
-});
-router.get('/sales', (req, res) => {
-    res.render('s_options', { 
-   } );
-});
-router.get('/talent', (req, res) => {
-    res.render('tm_options', { 
-   } );
-});
 
 router.get('/:team', (req, res) => {
     if(req.params.team == 'hr'){
