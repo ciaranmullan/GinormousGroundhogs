5384cc403b097f8cb10e910adad6992dd3cb3d65
Callum Kindred <callum-luis.kindred@kainos.com>
2021-08-12 12:18:11 +0100

    fix routing issue

--- app/routes.js
+++ app/routes.js
@@ -2,6 +2,12 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
+router.get('/employee_by_department', async (req, res) => { 
+    res.render('employee_by_department', { employees: await employeedata.getEmployeesByDepartment() } );
+    testTable = employeedata.getEmployeesByDepartment() 
+    console.log(testTable); 
+});
+
 router.get('/createemployee', (req, res) => {
     res.render('create_employee')
 })
@@ -40,8 +46,3 @@ module.exports = router
 
 const employeedata = require('./employeedata.js')
 
-router.get('/employee_by_department', async (req, res) => { 
-    res.render('employee_by_department', { employees: await employeedata.getEmployeesByDepartment() } );
-    testTable = employeedata.getEmployeesByDepartment() 
-    console.log(testTable); 
-});
