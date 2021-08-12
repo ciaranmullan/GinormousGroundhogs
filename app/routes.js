19b406236595a172affccc48552ef92d5e267a4e
Callum Kindred <callum-luis.kindred@kainos.com>
2021-08-12 10:35:33 +0100

    create front-end for use case 1

--- app/routes.js
+++ app/routes.js
@@ -2,5 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
+router.get('/createemployee', (req, res) => {
+    res.render('create_employee')
+})
 
+router.get('/employeeanswers', (req, res) => {
+    res.render('employee_check_answers')
+})
 module.exports = router
