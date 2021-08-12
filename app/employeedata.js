8f326fef216731b15d69b335b74327a3014ed3bb
Ciaran Mullan <ciaran.mullan@kainos.com>
2021-08-12 11:29:36 +0100

    Use case 2

---
+++ app/employeedata.js
@@ -0,0 +1,30 @@
+const mysql = require('mysql'); 
+const dbconfig = require('./dbconfig.json'); 
+const util = require ('util');
+const db = wrapDB(dbconfig);
+
+
+ function wrapDB (dbconfig) { 
+    const pool = mysql.createPool(dbconfig) 
+    return { 
+        query(sql, args) { 
+            console.log("in query in wrapper") 
+            return util.promisify( pool.query ) 
+            .call(pool, sql, args) 
+        }, 
+        release () { 
+            return util.promisify( pool.releaseConnection ) 
+            .call( pool ) 
+        } 
+    } 
+}
+
+exports.getEmployeesByDepartment = async () => { 
+    return await db.query( 
+        "SELECT empID, empName, department" 
+        + " FROM employees GROUP BY department, empID LIMIT 5" ) 
+ }
+
+ exports.getEmployees = () => { 
+     return this.employees(); 
+    }
--- app/routes.js
+++ app/routes.js
@@ -4,3 +4,11 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 module.exports = router
+
+const employeedata = require('./employeedata.js')
+
+router.get('/employee_by_department', async (req, res) => { 
+    res.render('employee_by_department', { employees: await employeedata.getEmployeesByDepartment() } );
+    testTable = employeedata.getEmployeesByDepartment() 
+    console.log(testTable); 
+});
