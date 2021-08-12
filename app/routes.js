68de7240e6ee429c06ff457b62570d19aab939ac
Callum Kindred <callum-luis.kindred@kainos.com>
2021-08-12 12:28:36 +0100

    merge fixes

--- app/routes.js
+++ app/routes.js
@@ -2,6 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
+
+router.get('/sales_emp_check_answers', (req, res) => {
+    res.render('sales_emp_check_answers')
+});
+
 router.get('/employee_by_department', async (req, res) => { 
     res.render('employee_by_department', { employees: await employeedata.getEmployeesByDepartment() } );
     testTable = employeedata.getEmployeesByDepartment() 
@@ -10,20 +15,19 @@ router.get('/employee_by_department', async (req, res) => {
 
 router.get('/createemployee', (req, res) => {
     res.render('create_employee')
-})
+});
 
 
 router.get('/employeeanswers', (req, res) => {
+    console.log("check answers")
     res.render('employee_check_answers')
-})
+});
 
 router.get('/createsalesemp', (req, res) => {
     res.render('create_sales_employee')
-})
+});
 
-router.get('/sales_emp_check_answers', (req, res) => {
-    res.render('sales_emp_check_answers')
-})
+
 
 router.get('/:team', (req, res) => {
     if(req.params.team == 'hr'){
